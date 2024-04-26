18a95f82ea7d610cd05a46ad45dd7c7ede2bdbad
Milica Gareski <milicakiprovska77@gmail.com>
2024-04-26 14:07:49 +0200

    git ignore added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const server = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.use(express.json());
 server.use(cors());
